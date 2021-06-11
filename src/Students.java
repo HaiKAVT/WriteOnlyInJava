0d1a83d7df757468d37d15bb2d899764eda862f2
HaiKAVT <haivtka@gmail.com>
2021-06-11 13:46:47 +0700

    setter

---
+++ src/Students.java
@@ -0,0 +1,37 @@
+public class Students {
+    private String name = "Jonh";
+    private String classes = "C02";
+    Students(){
+
+    }
+
+    public Students(String name, String classes) {
+        this.name = name;
+        this.classes = classes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+    @Override
+    public String toString() {
+        return "Students{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                '}';
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+
+        Students students1 = new Students();
+        students1.setName("Hải");
+        students1.setClasses("C04");
+        System.out.println(students1);
+
+    }
+}
